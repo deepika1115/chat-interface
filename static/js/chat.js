a3d577c946db0f60a0fe9030a9343655b75c0845
deepika <deepikapareek91@gmail.com>
2017-05-16 17:41:42 +0530

    make enter key responsive to send messages.

--- static/js/chat.js
+++ static/js/chat.js
@@ -28,3 +28,16 @@ $scope.records = [];
   }
 	
 });
+app.directive('ngEnter', function() {
+        return function(scope, element, attrs) {
+            element.bind("keydown keypress", function(event) {
+                if(event.which === 13) {
+                    scope.$apply(function(){
+                        scope.$eval(attrs.ngEnter, {'event': event});
+                    });
+
+                    event.preventDefault();
+                }
+            });
+        };
+    });
