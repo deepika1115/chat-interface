10a3ac74d00b5a0b9a38e49057503743639ae3ea
deepika <deepikapareek91@gmail.com>
2017-05-19 15:36:19 +0530

    save messages in localstorage.

--- static/js/chat.js
+++ static/js/chat.js
@@ -1,4 +1,3 @@
-
 var app = angular.module('myApp', ['ngMaterial', 'LocalStorageModule']);
 app.config(function (localStorageServiceProvider) {
   localStorageServiceProvider
@@ -6,6 +5,9 @@ app.config(function (localStorageServiceProvider) {
 }); 
 app.controller("myCtrl",function($scope, localStorageService){
 	$scope.today = new Date(); 
+
+    
+
 	var localData = localStorageService.get('localData');
     $scope.data = localData || {
 		myName : "",
@@ -17,17 +19,20 @@ app.controller("myCtrl",function($scope, localStorageService){
 	$scope.save = function() {
 		localStorageService.set('localData',$scope.data);
 	}
-	
-       
-
-$scope.records = [];
-  $scope.text;
-  $scope.func = function(text){
-    $scope.records.push(text);
-    $scope.text = "";
+    
+    
+    var msgData = localStorageService.get('msgData');
+    $scope.records = msgData || [];
+    $scope.text;
+    
+    $scope.func = function(text){
+        $scope.records.push(text);
+        localStorageService.set('msgData',$scope.records);
+        $scope.text = "";
   }
-	
+
 });
+
 app.directive('ngEnter', function() {
         return function(scope, element, attrs) {
             element.bind("keydown keypress", function(event) {
